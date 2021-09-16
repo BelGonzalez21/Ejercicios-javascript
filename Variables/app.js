3a52c321d313a72e920719ccd8ba57d779bc9e80
BelGonzalez21 <89754998+BelGonzalez21@users.noreply.github.com>
2021-09-16 14:13:17 -0300

    primer commit

---
+++ Variables/app.js
@@ -0,0 +1,52 @@
+//Bienvenida
+/*
+var PrimerNombre = prompt("Ingresa tu nombre");
+var apellido = prompt("Ingresa tu apellido");
+
+alert("Hola, " + PrimerNombre + " " + apellido);
+//Suma
+var primerNumero = prompt("Ingresa el primer numero: "); 
+var segundoNumero = prompt("Ingresa el segundo numero: ");
+var resultado = parseInt(primerNumero) + parseInt(segundoNumero);
+alert("Tu resultado es: " + resultado);
+//Año de nacimiento
+var edad = prompt("Ingresa tu edad: "); 
+var esteAño = 2021
+var resultado = parseInt(esteAño) - parseInt(edad);
+alert("Usted nació en: " + resultado);
+*/
+//Balance de cuenta
+/*
+var saldo = 15000
+alert("Usted dispone de: " + saldo);
+var extraccion = prompt("¿Cuanto desea retirar?: "); 
+var resultado = parseInt(saldo) - parseInt(extraccion);
+alert("Su nuevo saldo es de: " + resultado)
+*/
+//4) Solicitar al usuario 3 notas, calcular el promedio y mostrarlo por consola.
+/*
+var nota1 = prompt("Ingrese primer nota");
+var nota2 = prompt("Ingrese segunda nota");
+var nota3 = prompt("Ingrese tercera nota");
+var suma = parseInt(nota1) + parseInt(nota2) + parseInt(nota3);
+var resultado = parseInt(suma) / 3
+alert("Su promedio es: " + resultado)
+*/
+//5) Crear un conversor de medidas. Pedirle al usuario que ingrese una medida en metros y pasarlo a pies, centimetros y pulgadas. Mostrar por consola los tres resultados.
+'metro': {
+    'metro' = 1,
+    'centimetro' = 100,
+    'pies' = 3.28084,
+    'pulgadas' = 39.37008
+},
+var metro = prompt("Ingrese su medida en metros");
+var resultado = parseInt(metro) * parseInt(centimetro); parseFloat(pies); parseFloat(pulgadas)
+alert("Las unidades son: " + resultado)
+
+
+
+
+
+
+
+
